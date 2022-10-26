a1a589a26426f01ba2fec79fdea85050f035657d
loofeht <phu832001@gmail.com>
2022-10-26 15:12:04 +0700

    test

--- index.php
+++ index.php
@@ -20,8 +20,7 @@
                 <nav class="navbar">
                     <div class="container-fluid">
                         <div class="navbar-header">
-                            <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt=""
-                                    class="img-fluid"></a>
+                            <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
                         </div>
                         <ul class="nav navbar-nav">
                             <li><a href="#search-map">Tìm đường</a></li>
@@ -43,7 +42,10 @@
                         <div class="row search-map-inner">
                             <div class="search-map-left col-lg-8">
                                 <div class="search-map-left-inner">
-                                    <div id="map" class="map"></div>
+                                    <div id="map" class="map">
+
+
+                                    </div>
                                 </div>
                             </div>
                             <div class="search-map-right col-lg-4">
@@ -64,7 +66,7 @@
                                                     <option value="TLU">Đại học Thủy Lợi</option>
                                                     <option value="LDA">Đại học Công Đoàn</option>
                                                     <option value="VWA">Học viện Phụ nữ Việt Nam</option>
-                                                    <option value="FBU">Đại học Ngân Hàng</option>
+                                                    <option value="FBU">Học viện Ngân Hàng</option>
                                                 </select>
                                             </div>
                                             <div class="form-group">
@@ -103,5 +105,88 @@
 <script src="assets/js/jquery-3.6.1.js"></script>
 <script src="assets/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
 <script src="assets/js/javascript.js"></script>
+
+<script type="text/javascript">
+    var split, result;
+    $("#document").ready(function() {
+        var startPoint = new ol.Feature();
+        var destPoint = new ol.Feature();
+        var format = 'image/png';
+        var bounds = [105.671539306641, 20.8914451599121,
+            105.982925415039, 21.186128616333
+        ];
+        split = new ol.layer.Image({
+            source: new ol.source.ImageWMS({
+                url: 'http://localhost:8080/geoserver/btl/wms',
+                params: {
+                    'FORMAT': format,
+                    'VERSION': '1.1.1',
+                    STYLES: '',
+                    LAYERS: 'split',
+                }
+            })
+        });
+        var projection = new ol.proj.Projection({
+            code: 'EPSG:4326',
+            units: 'degrees',
+            axisOrientation: 'neu'
+        });
+        var view = new ol.View({
+            projection: projection
+        });
+        var map = new ol.Map({
+            target: 'map',
+            layers: [
+                split
+            ],
+            view: view
+        });
+        //map.getView().fitExtent(bounds, map.getSize());
+        map.getView().fit(bounds, map.getSize());
+        var vectorLayer = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                features: [startPoint, destPoint]
+            })
+        });
+        map.addLayer(vectorLayer);
+        map.on('singleclick', function(evt) {
+            if (startPoint.getGeometry() == null) {
+                // First click.
+                startPoint.setGeometry(new ol.geom.Point(evt.coordinate));
+                $("#txtPoint1").val(evt.coordinate);
+            } else if (destPoint.getGeometry() == null) {
+                // Second click.
+                destPoint.setGeometry(new ol.geom.Point(evt.coordinate));
+                $("#txtPoint2").val(evt.coordinate);
+            }
+        });
+        $("#btnSolve").click(function() {
+            var startCoord = startPoint.getGeometry().getCoordinates();
+            var destCoord = destPoint.getGeometry().getCoordinates();
+            var params = {
+                LAYERS: 'route2',
+                FORMAT: 'image/png'
+            };
+            var viewparams = [
+                'x1:' + startCoord[0], 'y1:' + startCoord[1],
+                'x2:' + destCoord[0], 'y2:' + destCoord[1]
+            ];
+            params.viewparams = viewparams.join(';');
+            result = new ol.layer.Image({
+                source: new ol.source.ImageWMS({
+                    url: 'http://localhost:8080/geoserver/btl/wms',
+                    params: params
+                })
+            });
+            map.addLayer(result);
+        });
+        $("#btnReset").click(function() {
+            startPoint.setGeometry(null);
+            destPoint.setGeometry(null);
+            // Remove the result layer.
+            map.removeLayer(result);
+        });
+    });
+</script>
 
 </html>
