773ce9c11fc78e7d502a316cfd8b54ad57c90da5
loofeht <phu832001@gmail.com>
2022-10-30 14:50:35 +0700

    fixed find map

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
                 <nav class="navbar">
                     <div class="container-fluid">
                         <div class="navbar-header">
-                            <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt=""
-                                    class="img-fluid"></a>
+                            <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
                         </div>
                         <ul class="nav navbar-nav">
                             <li><a href="#search-map">Tìm đường</a></li>
@@ -61,15 +60,15 @@
                                                 </select>
                                             </div>
                                             <script>
-                                            function uniChanged(obj) {
-                                                var value = obj.value;
-                                                if (value != '') {
-                                                    value = value.split("-");
-                                                    var toado = value[0];
-                                                    var gid = value[1];
-                                                    $("#test").text(gid);
-                                                } else $("#test").text('chưa chọn');
-                                            }
+                                                function uniChanged(obj) {
+                                                    var value = obj.value;
+                                                    if (value != '') {
+                                                        value = value.split("-");
+                                                        var toado = value[0];
+                                                        var gid = value[1];
+                                                        $("#test").text(gid);
+                                                    } else $("#test").text('chưa chọn');
+                                                }
                                             </script>
                                         </div>
                                         <div class="search-btn">
